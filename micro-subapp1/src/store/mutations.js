515806e93848b5583cf262aa71bd1443e3205c40
tianjing <tianjing@xd.com>
2020-09-08 17:59:07 +0800

    update

--- micro-subapp1/src/store/mutations.js
+++ micro-subapp1/src/store/mutations.js
@@ -1,6 +1,6 @@
 const mutations = {
   setPropsFromMainApp (state, payload) {
-    // state.propsFromMainApp = payload
+    state.propsFromMainApp = payload
     console.log(payload, '基座传递微应用的数据')
   }
 }
